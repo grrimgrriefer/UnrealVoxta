c10bd4e52accf4748c7f1c9f9d262cb9ff51ae4d
grrimgrriefer <168567347+grrimgrriefer@users.noreply.github.com>
2024-10-26 23:08:27 +0200

    changed test pattern to avoid warnings bleeding into other tests

--- Tests/VoxtaClientTests.cpp
+++ Tests/VoxtaClientTests.cpp
@@ -4,6 +4,7 @@
 #include "TestLogSink.h"
 #include "CQTest.h"
 #include "VoxtaClient.h"
+#include "VoxtaClientState.h"
 #include "AiCharData.h"
 #include "UserCharData.h"
 #include "ChatMessage.h"
@@ -27,6 +28,8 @@ TEST_CLASS(VoxtaClientTests, "Voxta")
 	VoxtaClientState m_newStateResponse;
 	TArray<FAiCharData> m_characters;
 	TArray<TTuple<FBaseCharData, FChatMessage>> m_messages;
+
+	VoxtaClientState m_cache_state;
 
 	/** Create a new clean m_gameInstance and voxtaclient for ever test, to avoid inter-test false positives. */
 	BEFORE_EACH()
@@ -63,14 +66,17 @@ TEST_CLASS(VoxtaClientTests, "Voxta")
 		TestRunner->SetSuppressLogWarnings();
 
 		m_voxtaClient->VoxtaClientStateChangedEventNative.Remove(m_stateChangedEventHandle);
-		m_voxtaClient->VoxtaClientStateChangedEventNative.Remove(m_characterRegisteredEventHandle);
-		m_voxtaClient->VoxtaClientStateChangedEventNative.Remove(m_charMessageAddedEventHandle);
+		m_voxtaClient->VoxtaClientCharacterRegisteredEventNative.Remove(m_characterRegisteredEventHandle);
+		m_voxtaClient->VoxtaClientCharMessageAddedEventNative.Remove(m_charMessageAddedEventHandle);
 		m_gameInstance->Shutdown();
 		m_voxtaClient = nullptr;
 		m_gameInstance = nullptr;
 
 		m_newStateResponse = VoxtaClientState::Disconnected;
+		m_characters.Empty();
+		m_messages.Empty();
 
+		GLog->Flush();
 		GLog->RemoveOutputDevice(m_testLogSink);
 		TestRunner->SetSuppressLogErrors(ECQTestSuppressLogBehavior::False);
 		TestRunner->SetSuppressLogWarnings(ECQTestSuppressLogBehavior::False);
@@ -90,70 +96,105 @@ TEST_CLASS(VoxtaClientTests, "Voxta")
 #pragma region StartConnection
 	TEST_METHOD(StartConnection_WithValidHost_NewStateAndBroadCast)
 	{
-		/** Test */
-		m_voxtaClient->StartConnection(FString("127.0.0.1"), 5384);
+		TestCommandBuilder.Do([this] ()
+		{
+			/** Test */
+			m_voxtaClient->StartConnection(FString("127.0.0.1"), 5384);
+		});
 
-		/** Assert */
-		GLog->Flush();
-		ASSERT_THAT(AreEqual(m_newStateResponse, VoxtaClientState::AttemptingToConnect));
-		ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), VoxtaClientState::AttemptingToConnect));
+		AddCommand(WaitForDuration(0.1));
+		TestCommandBuilder.Do([this] ()
+		{
+			/** Assert */
+			GLog->Flush();
+			ASSERT_THAT(AreEqual(m_newStateResponse, VoxtaClientState::AttemptingToConnect));
+			ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), VoxtaClientState::AttemptingToConnect));
+		});
 	}
 
 	TEST_METHOD(StartConnection_WithEmptyAddress_ExpectDisconnectedAndErrorLog)
 	{
-		/** Setup */
-		TestRunner->SetSuppressLogErrors();
+		TestCommandBuilder.Do([this] ()
+		{
+			/** Setup */
+			TestRunner->SetSuppressLogErrors();
 
-		/** Test */
-		m_voxtaClient->StartConnection(FString(), 5384);
+			/** Test */
+			m_voxtaClient->StartConnection(FString(), 5384);
+		});
 
-		/** Assert */
-		GLog->Flush();
-		ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), VoxtaClientState::Disconnected));
-		ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(TEXT("address"), ELogVerbosity::Type::Error)));
+		AddCommand(WaitForDuration(0.1));
+		TestCommandBuilder.Do([this] ()
+		{
+			/** Assert */
+			GLog->Flush();
+			ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), VoxtaClientState::Disconnected));
+			ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(TEXT("address"), ELogVerbosity::Type::Error)));
+		});
 	}
 
 	TEST_METHOD(StartConnection_WithInvalidAddress_ExpectDisconnectedAndErrorLog)
 	{
-		/** Setup */
-		TestRunner->SetSuppressLogErrors();
+		TestCommandBuilder.Do([this] ()
+		{
+			/** Setup */
+			TestRunner->SetSuppressLogErrors();
 
-		/** Test */
-		m_voxtaClient->StartConnection(FString("1.2.3.4.5"), 5384);
+			/** Test */
+			m_voxtaClient->StartConnection(FString("1.2.3.4.5"), 5384);
+		});
 
-		/** Assert */
-		GLog->Flush();
-		ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), VoxtaClientState::Disconnected));
-		ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(TEXT("address"), ELogVerbosity::Type::Error)));
+		AddCommand(WaitForDuration(0.1));
+		TestCommandBuilder.Do([this] ()
+		{
+			/** Assert */
+			GLog->Flush();
+			ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), VoxtaClientState::Disconnected));
+			ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(TEXT("address"), ELogVerbosity::Type::Error)));
+		});
 	}
 
 	TEST_METHOD(StartConnection_WithInvalidPort_ExpectDisconnectedAndErrorLog)
 	{
 		/** Setup */
-		TestRunner->SetSuppressLogErrors();
 		int port = 100000;
+		TestCommandBuilder.Do([this, port] ()
+		{
+			TestRunner->SetSuppressLogErrors();
 
-		/** Test */
-		m_voxtaClient->StartConnection(FString("127.0.0.1"), port);
+			/** Test */
+			m_voxtaClient->StartConnection(FString("127.0.0.1"), port);
+		});
 
-		/** Assert */
-		GLog->Flush();
-		ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), VoxtaClientState::Disconnected));
-		ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(FString::FromInt(port), ELogVerbosity::Type::Error)));
+		AddCommand(WaitForDuration(0.1));
+		TestCommandBuilder.Do([this, port] ()
+		{
+			/** Assert */
+			GLog->Flush();
+			ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), VoxtaClientState::Disconnected));
+			ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(FString::FromInt(port), ELogVerbosity::Type::Error)));
+		});
 	}
 
 	TEST_METHOD(StartConnection_WithInvalidStartingState_ExpectIgnoredAndWarningLog)
 	{
 		/** Setup */
 		PreconfigureClient(PreconfigureClientState::Started);
-		TestRunner->SetSuppressLogWarnings();
+		TestCommandBuilder.Do([this] ()
+		{
+			TestRunner->SetSuppressLogWarnings();
 
-		/** Test */
-		m_voxtaClient->StartConnection(FString("127.0.0.1"), 5384);
+			/** Test */
+			m_voxtaClient->StartConnection(FString("127.0.0.1"), 5384);
+		});
 
-		/** Assert */
-		GLog->Flush();
-		ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(TEXT("ignoring new connection attempt"), ELogVerbosity::Type::Warning)));
+		AddCommand(WaitForDuration(0.1));
+		TestCommandBuilder.Do([this] ()
+		{
+			/** Assert */
+			GLog->Flush();
+			ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(TEXT("ignoring new connection attempt"), ELogVerbosity::Type::Warning)));
+		});
 	}
 
 	TEST_METHOD(StartConnection_WaitForCharacterRegisteredEvent_ExpectEventTriggeredBeforeTimeout)
@@ -165,16 +206,17 @@ TEST_CLASS(VoxtaClientTests, "Voxta")
 		/** Test */
 		AddCommand(new FWaitUntil(*TestRunner, [&] () { return m_voxtaClient->GetCurrentState() == VoxtaClientState::Idle; }, timeout));
 
+		AddCommand(WaitForDuration(0.1));
 		TestCommandBuilder.Do([this] ()
+		{
+			/** Assert */
+			GLog->Flush();
+			ASSERT_THAT(IsTrue(m_characters.Num() > 0));
+			for (int i = 0; i < m_characters.Num(); i++)
 			{
-				/** Assert */
-				GLog->Flush();
-				ASSERT_THAT(IsTrue(m_characters.Num() > 0));
-				for (int i = 0; i < m_characters.Num(); i++)
-				{
-					ASSERT_THAT(IsNotNull(m_characters[i].GetName()));
-				}
-			});
+				ASSERT_THAT(IsNotNull(m_characters[i].GetName()));
+			}
+		});
 	}
 #pragma endregion
 
@@ -183,42 +225,62 @@ TEST_CLASS(VoxtaClientTests, "Voxta")
 	{
 		/** Setup */
 		PreconfigureClient(PreconfigureClientState::Started);
+		TestCommandBuilder.Do([this] ()
+		{
+			/** Test */
+			m_voxtaClient->Disconnect();
+		});
 
-		/** Test */
-		m_voxtaClient->Disconnect();
-
-		/** Assert */
-		GLog->Flush();
-		ASSERT_THAT(AreEqual(m_newStateResponse, VoxtaClientState::Terminated));
-		ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), VoxtaClientState::Terminated));
+		AddCommand(WaitForDuration(0.1));
+		TestCommandBuilder.Do([this] ()
+		{
+			/** Assert */
+			GLog->Flush();
+			ASSERT_THAT(AreEqual(m_newStateResponse, VoxtaClientState::Terminated));
+			ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), VoxtaClientState::Terminated));
+		});
 	}
 
 	TEST_METHOD(Disconnect_WithoutHavingConnected_ExpectedIgnoredAndWarningLog)
 	{
-		/** Setup */
-		TestRunner->SetSuppressLogWarnings();
+		TestCommandBuilder.Do([this] ()
+		{
+			/** Setup */
+			TestRunner->SetSuppressLogWarnings();
 
-		/** Test */
-		m_voxtaClient->Disconnect();
+			/** Test */
+			m_voxtaClient->Disconnect();
+		});
 
-		/** Assert */
-		GLog->Flush();
-		ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(TEXT("ignoring disconnect attempt"), ELogVerbosity::Type::Warning)));
+		AddCommand(WaitForDuration(0.1));
+		TestCommandBuilder.Do([this] ()
+		{
+			/** Assert */
+			GLog->Flush();
+			ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(TEXT("ignoring disconnect attempt"), ELogVerbosity::Type::Warning)));
+		});
 	}
 
 	TEST_METHOD(Disconnect_WithSilentEnabled_StateRemainsUnchanged)
 	{
 		/** Setup */
 		PreconfigureClient(PreconfigureClientState::Started);
-		VoxtaClientState state = m_voxtaClient->GetCurrentState();
+		TestCommandBuilder.Do([this] ()
+		{
+			m_cache_state = m_voxtaClient->GetCurrentState();
 
-		/** Test */
-		m_voxtaClient->Disconnect(true);
+			/** Test */
+			m_voxtaClient->Disconnect(true);
+		});
 
-		/** Assert */
-		GLog->Flush();
-		ASSERT_THAT(AreEqual(m_newStateResponse, state));
-		ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), state));
+		AddCommand(WaitForDuration(0.1));
+		TestCommandBuilder.Do([this] ()
+		{
+			/** Assert */
+			GLog->Flush();
+			ASSERT_THAT(AreEqual(m_newStateResponse, m_cache_state));
+			ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), m_cache_state));
+		});
 	}
 #pragma endregion
 
@@ -227,17 +289,24 @@ TEST_CLASS(VoxtaClientTests, "Voxta")
 	{
 		/** Setup */
 		FString characterId = FGuid::NewGuid().ToString();
-		VoxtaClientState state = m_voxtaClient->GetCurrentState();
-		TestRunner->SetSuppressLogErrors();
+		TestCommandBuilder.Do([this, characterId] ()
+		{
+			m_cache_state = m_voxtaClient->GetCurrentState();
+			TestRunner->SetSuppressLogErrors();
 
-		/** Test */
-		m_voxtaClient->StartChatWithCharacter(characterId);
+			/** Test */
+			m_voxtaClient->StartChatWithCharacter(characterId);
+		});
 
-		/** Assert */
-		GLog->Flush();
-		ASSERT_THAT(AreEqual(m_newStateResponse, state));
-		ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), state));
-		ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(characterId, ELogVerbosity::Type::Error)));
+		AddCommand(WaitForDuration(0.1));
+		TestCommandBuilder.Do([this, characterId] ()
+		{
+			/** Assert */
+			GLog->Flush();
+			ASSERT_THAT(AreEqual(m_newStateResponse, m_cache_state));
+			ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), m_cache_state));
+			ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(characterId, ELogVerbosity::Type::Error)));
+		});
 	}
 
 	TEST_METHOD(StartChatWithCharacter_WithEmptyString_ExpectErrorAndStateRemainsUnchanged)
@@ -247,16 +316,20 @@ TEST_CLASS(VoxtaClientTests, "Voxta")
 		TestCommandBuilder.Do([this] ()
 		{
 			FString characterId = FString();
-			VoxtaClientState state = m_voxtaClient->GetCurrentState();
+			m_cache_state = m_voxtaClient->GetCurrentState();
 			TestRunner->SetSuppressLogErrors();
 
 			/** Test */
 			m_voxtaClient->StartChatWithCharacter(characterId);
+		});
 
+		AddCommand(WaitForDuration(0.1));
+		TestCommandBuilder.Do([this] ()
+		{
 			/** Assert */
 			GLog->Flush();
-			ASSERT_THAT(AreEqual(m_newStateResponse, state));
-			ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), state));
+			ASSERT_THAT(AreEqual(m_newStateResponse, m_cache_state));
+			ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), m_cache_state));
 			ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(TEXT("empty"), ELogVerbosity::Type::Error)));
 		});
 	}
@@ -269,7 +342,11 @@ TEST_CLASS(VoxtaClientTests, "Voxta")
 		{
 			/** Test */
 			m_voxtaClient->StartChatWithCharacter(m_characters[0].GetId());
+		});
 
+		AddCommand(WaitForDuration(0.1));
+		TestCommandBuilder.Do([this] ()
+		{
 			/** Assert */
 			GLog->Flush();
 			ASSERT_THAT(AreEqual(m_newStateResponse, VoxtaClientState::StartingChat));
@@ -290,6 +367,7 @@ TEST_CLASS(VoxtaClientTests, "Voxta")
 		/** Test */
 		AddCommand(new FWaitUntil(*TestRunner, [&] () { return m_voxtaClient->GetCurrentState() == VoxtaClientState::WaitingForUserReponse; }, timeout));
 
+		AddCommand(WaitForDuration(0.1));
 		TestCommandBuilder.Do([this] ()
 		{
 			/** Assert */
@@ -305,19 +383,26 @@ TEST_CLASS(VoxtaClientTests, "Voxta")
 #pragma region SendUserInput
 	TEST_METHOD(SendUserInput_WithInvalidStartingState_ExpectErrorAndStateRemainsUnchanged)
 	{
-		/** Setup */
 		FString text = TEXT("some user text");
-		VoxtaClientState state = m_voxtaClient->GetCurrentState();
-		TestRunner->SetSuppressLogErrors();
+		TestCommandBuilder.Do([this, text] ()
+		{
+			/** Setup */
+			m_cache_state = m_voxtaClient->GetCurrentState();
+			TestRunner->SetSuppressLogErrors();
 
-		/** Test */
-		m_voxtaClient->SendUserInput(text);
+			/** Test */
+			m_voxtaClient->SendUserInput(text);
+		});
 
-		/** Assert */
-		GLog->Flush();
-		ASSERT_THAT(AreEqual(m_newStateResponse, state));
-		ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), state));
-		ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(text, ELogVerbosity::Type::Error)));
+		AddCommand(WaitForDuration(0.1));
+		TestCommandBuilder.Do([this, text] ()
+		{
+			/** Assert */
+			GLog->Flush();
+			ASSERT_THAT(AreEqual(m_newStateResponse, m_cache_state));
+			ASSERT_THAT(AreEqual(m_voxtaClient->GetCurrentState(), m_cache_state));
+			ASSERT_THAT(IsTrue(m_testLogSink->ContainsLogMessageWithSubstring(text, ELogVerbosity::Type::Error)));
+		});
 	}
 
 	TEST_METHOD(SendUserInput_ExpectStateChange)
@@ -328,7 +413,11 @@ TEST_CLASS(VoxtaClientTests, "Voxta")
 		{
 			/** Test */
 			m_voxtaClient->SendUserInput(TEXT("some user text"));
+		});
 
+		AddCommand(WaitForDuration(0.1));
+		TestCommandBuilder.Do([this] ()
+		{
 			/** Assert */
 			GLog->Flush();
 			ASSERT_THAT(AreEqual(m_newStateResponse, VoxtaClientState::GeneratingReply));
@@ -350,6 +439,7 @@ TEST_CLASS(VoxtaClientTests, "Voxta")
 		/** Test */
 		AddCommand(new FWaitUntil(*TestRunner, [&] () { return m_voxtaClient->GetCurrentState() == VoxtaClientState::WaitingForUserReponse; }, timeout));
 
+		AddCommand(WaitForDuration(0.1));
 		TestCommandBuilder.Do([this, text] ()
 		{
 			/** Assert */
@@ -372,7 +462,10 @@ TEST_CLASS(VoxtaClientTests, "Voxta")
 
 	void PreconfigureClient(PreconfigureClientState state)
 	{
-		m_voxtaClient->StartConnection(FString("127.0.0.1"), 5384);
+		TestCommandBuilder.Do([this] ()
+		{
+			m_voxtaClient->StartConnection(FString("127.0.0.1"), 5384);
+		});
 		if (state == PreconfigureClientState::Started)
 		{
 			return;
@@ -387,6 +480,15 @@ TEST_CLASS(VoxtaClientTests, "Voxta")
 			m_voxtaClient->StartChatWithCharacter(m_characters[0].GetId());
 		});
 		AddCommand(new FWaitUntil(*TestRunner, [&] () { return m_voxtaClient->GetCurrentState() == VoxtaClientState::WaitingForUserReponse; }));
+	}
+
+	FWaitUntil* WaitForDuration(double seconds)
+	{
+		FDateTime endTime = FDateTime::Now() + FTimespan::FromSeconds(seconds);
+		return new FWaitUntil(*TestRunner, [&] ()
+			{
+				return FDateTime::Now() > endTime;
+			}, FTimespan::FromSeconds(seconds + 5));
 	}
 #pragma endregion
 };
